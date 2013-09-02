fb85292cb710bd169b19223c8288fb6ef455ff91
akama <adam@nexadmin.com>
2013-09-01 23:32:42 -0500

    little template changes

--- CTF/views.py
+++ CTF/views.py
@@ -29,7 +29,7 @@ def register(request):
             return HttpResponseRedirect(reverse('home'))
     else:
         form = UserCreationForm()
-    return render(request, "registration/register.html", {
+    return render(request, "registration/login.html", {
         'form': form,
     })
 
