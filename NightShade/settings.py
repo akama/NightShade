1a28b41bb118b5bfc9aeb7c19cfa2db5e4353040
Akama <adam@nexadmin.com>
2013-09-02 17:44:44 -0500

    couldn't read, fixing database settings.

--- NightShade/settings.py
+++ NightShade/settings.py
@@ -14,6 +14,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+		    'default': {
+			            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+			            'NAME': 'MyDatebase', # Or path to database file if using sqlite3.
+			            # The following settings are not used with sqlite3:
+			            'USER': '',
+				    'PASSWORD': '',
+				    'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		   	            'PORT': '', # Set to empty string for default.
+			        }
+	    }
+
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PRONTO', 'https')
