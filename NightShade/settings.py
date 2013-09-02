f0f463836b4c286d6e7889fa0827f0ebf526cd94
akama <adam@nexadmin.com>
2013-09-02 17:10:47 -0500

    changes to fix heroku

--- NightShade/settings.py
+++ NightShade/settings.py
@@ -65,7 +65,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
+
+# Static asset configuration
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -76,7 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join('static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
--- NightShade/wsgi.py
+++ NightShade/wsgi.py
@@ -25,8 +25,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NightShade.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
