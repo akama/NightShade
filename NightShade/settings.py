a2efd02aad22a0b9b97d620075541fb5b6201792
Akama <adam@nexadmin.com>
2013-09-04 19:40:51 -0500

    quickly

--- NightShade/settings.py
+++ NightShade/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Django settings for NightShade project.
 
@@ -14,24 +15,20 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-		    'default': {
-			            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-				            'NAME': 'MyDatebase', # Or path to database file if using sqlite3.
-					            # The following settings are not used with sqlite3:
-						            'USER': '',
-							            'PASSWORD': '',
-								            'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-									            'PORT': '', # Set to empty string for default.
-										        }
-		    }
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'mydatabase'
+	        }
+	    }
 
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PRONTO', 'https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -163,4 +160,3 @@ LOGGING = {
 												        },
 						        }
 				    }
-
