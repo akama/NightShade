e094c2cc1ac9c9386acb5906786718693e18844d
B0bby <unthinkingdepths+git@gmail.com>
2013-09-05 16:45:28 -0500

    CSS changes for tiny viewport compaitibility

--- NightShade/settings.py
+++ NightShade/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
 	    }
 
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PRONTO', 'https')
 
