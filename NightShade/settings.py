3d312bca916027f11d8c034ceed33f004972f99a
Adam Ringwood <adam@nexadmin.com>
2014-03-19 19:20:50 -0500

    fixing databases

--- NightShade/settings.py
+++ NightShade/settings.py
@@ -14,7 +14,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PRONTO', 'https')
 
